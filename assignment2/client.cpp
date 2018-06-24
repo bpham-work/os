e4533b399ac21f2262f104565f6da1528ab891b2
Bryant Pham <bryantpham@Bryants-MacBook-Pro.local>
2018-06-23 22:40:01 -0500

    assignment2/init

---
+++ assignment2/client.cpp
@@ -0,0 +1,134 @@
+// Please note this is a C program
+// It compiles without warnings with gcc
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <iostream> 
+#include "request.h"
+
+void error(char *msg) {
+    perror(msg);
+    exit(0);
+}
+
+char* sendRequest(int& sockfd, request& req) {
+    char response[100];
+    int result;
+    result = write(sockfd, &req, sizeof(req));
+    if (result < 0) { 
+        error("ERROR writing to socket");
+    }
+    bzero(response, 100);
+    result = read(sockfd, response, 100);
+    if (result < 0) {
+         error("ERROR reading from socket");
+    }
+    return response;
+}
+
+request buildRequest(int& requestType, int& acctNum) {
+    char buffer[100];
+    bzero(buffer, 100);
+    request req {requestType, acctNum};
+    int amt;
+    switch (requestType) {
+        case 2:
+            printf("Enter amount to deposit: ");
+            fgets(buffer, 256, stdin);
+            sscanf(buffer, "%d", &amt);
+            req.amt = amt;
+            break;
+        case 3:
+            printf("Enter amount to withdraw: ");
+            fgets(buffer, 256, stdin);
+            sscanf(buffer, "%d", &amt);
+            req.amt = amt;
+            break;
+        case 4:
+            break;
+    }
+    return req;
+}
+
+/**
+ * Prompts user to login via account number
+ * @returns Account number if successful login. -1 if unsuccessful login
+ */
+int login(int& sockfd) {
+    bool loginSuccess = false;
+    char buffer[256];
+    bzero(buffer, 256);
+    int acctNum;
+    char* SUCCESS_RESPONSE = "LOGIN_SUCCESS";
+    while (!loginSuccess) {
+        printf("Enter account number: ");
+        fgets(buffer, 256, stdin);
+        sscanf(buffer, "%d", &acctNum); // convert account number to int
+        request loginRequest {0, acctNum, 0};
+        char* loginResponse = sendRequest(sockfd, loginRequest);
+        printf("%s\n", loginResponse);
+        loginSuccess = strcmp(SUCCESS_RESPONSE, loginResponse) == 0;
+    }
+    return acctNum;
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd, portno, n;
+
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) {
+        error("ERROR connecting");
+    }
+
+    int acctNum = login(sockfd);
+    if (acctNum) {
+        int input;
+        while (true) {
+            bzero(buffer, 256);
+            printf("1. View balance\n");
+            printf("2. Deposit money\n");
+            printf("3. Withdraw money\n");
+            printf("4. Disconnect\n");
+            printf("Enter option: ");
+            fgets(buffer, 255, stdin);
+            sscanf(buffer, "%d", &input); // convert input to int
+            request req = buildRequest(input, acctNum);
+            char* response = sendRequest(sockfd, req);
+            if (req.requestType == 4) {
+                printf("Disconnecting session\n");
+                exit(0);
+            }
+            printf("%s", response);
+        }
+    }
+    return 0;
+}
+
---
+++ assignment2/server.cpp
@@ -0,0 +1,135 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <stdexcept>
+#include "request.h" 
+
+void error(char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+std::unordered_map<std::string, double> readAccounts(std::string filename) {
+    std::ifstream infile(filename);
+    if (infile.fail())
+        throw std::invalid_argument("Could not open file - invalid file name");
+    std::string record;
+    std::unordered_map<std::string, double> accounts;
+    while (getline(infile, record)) {
+        int firstSpace = record.find(" ");
+        int secondSpace = record.find(" ", firstSpace + 1);
+        std::string acctNum = record.substr(0, firstSpace);
+        std::string acctBal = record.substr(firstSpace + 1, secondSpace - firstSpace - 1);
+        accounts[acctNum] = std::stod(acctBal);
+    }
+    infile.close();
+    return accounts;
+}
+
+char* processRequest(std::unordered_map<std::string, double>& accounts, request& req) {
+    char response[100];
+    std::string acctNum = std::to_string(req.acctNum);
+    double origAmt;
+    double newBal;
+    switch (req.requestType) {
+        case 0:
+            if (accounts.find(acctNum) != accounts.end()) {
+                printf("Connecting account %d\n", req.acctNum);
+                sprintf(response, "LOGIN_SUCCESS");
+            } else {
+                sprintf(response, "LOGIN_FAILED");
+            }
+            break;
+        case 1:
+            sprintf(response, "Account number: %s\nBalance: $%f\n", acctNum.c_str(), accounts[acctNum]);
+            break;
+        case 2:
+            origAmt = accounts[acctNum];
+            newBal = accounts[acctNum] + req.amt;
+            accounts[acctNum] = newBal;
+            sprintf(response, "Account number: %s\nNew Balance: $%f\n", acctNum.c_str(), newBal);
+            break;
+        case 3:
+            if (req.amt <= accounts[acctNum]) {
+                origAmt = accounts[acctNum];
+                newBal = accounts[acctNum] - req.amt;
+                accounts[acctNum] = newBal;
+                sprintf(response, "Account number: %s\nNew Balance: $%f\n", acctNum.c_str(), newBal);
+            } else {
+                sprintf(response, "Insufficient funds!\n");
+            }
+            break;
+        case 4:
+            printf("Disconnecting account %d\n", req.acctNum);
+            break;
+    }
+    return response;
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd, newsockfd, portno, clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    if (argc < 3) {
+        fprintf(stderr,"ERROR - usage: %s [port] [inputFilename]\n", argv[0]);
+        exit(1);
+    }
+    std::unordered_map<std::string, double> accounts = readAccounts(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) { 
+        error("ERROR opening socket");
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    int bindRes = bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+    if (bindRes < 0) {
+        error("ERROR on binding");
+    }
+    int listenRes = listen(sockfd, 5);
+    if (listenRes == 0) {
+        printf("Listening...\n");
+    }
+    clilen = sizeof(cli_addr);
+
+    pid_t pid;
+    request req;
+    while (true) {
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *) &clilen);
+        if (newsockfd < 0) {
+            error("ERROR on accept");
+        }
+        pid = fork();
+        if (pid == 0) {
+            int rwBytes;
+            while (true) {
+                rwBytes = read(newsockfd, &req, sizeof(req));
+                if (rwBytes < 0) {
+                    error("ERROR reading from socket");
+                } else if (rwBytes > 0) {
+                    char* response = processRequest(accounts, req);
+                    rwBytes = write(newsockfd, response, 100);
+                    if (rwBytes < 0) {
+                        error("ERROR writing to socket");
+                    }
+                    if (req.requestType == 4) {
+                        exit(0);
+                    }
+                }
+            }
+        }
+    }
+
+    return 0; 
+}
+
