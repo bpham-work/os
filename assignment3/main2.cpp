1ae9fbb18e6e8a156294f27b8e411da9ecff49e3
Bryant Pham <bryantpham0@gmail.com>
2018-07-01 19:12:30 -0500

    assignment3/add service data

--- assignment3/main2.cpp
+++ assignment3/main2.cpp
@@ -45,19 +45,39 @@ int main(int argc, char* argv[]) {
     std::string filename = argv[2];
     vector<customer> customers = getCustomers(filename); 
     sem_t *clerkSem = sem_open("clerks", O_CREAT, 0600, numOfClerks);
+    sem_t *serviceDataSem = sem_open("serviceData", O_CREAT, 0600, 1);
     pid_t pid;
     int status = 0;
-    int numWaiting;
+    int semValue;
+
+    key_t key = 6428;
+    int shmid = shmget(key, sizeof(int) * 3, IPC_CREAT | 0666);
+    int* totalServiced = (int*) shmat(shmid, NULL, 0);
+    int* numWaited = totalServiced + 1;
+    int* numNotWaited = numWaited + 1;
+    shmctl(shmid, IPC_RMID, NULL);
     for (int i = 0; i < customers.size(); i++) {
         sleep(customers[i].timeLastArrival);
         clerkSem = sem_open("clerks", O_CREAT);
         if ((pid = fork()) == 0) {
             printf("%s arriving\n", customers[i].name.c_str());
-            if (sem_getvalue(clerkSem, &numWaiting)) {
+            
+	    // Record service data
+	    sem_wait(serviceDataSem);
+	    if (sem_getvalue(clerkSem, &semValue)) {
                 perror("getval");
                 return 1;
             }
-            printf("WAIT VAL %d\n", numWaiting);
+	    (*totalServiced)++;
+	    if (semValue > 0) {
+		(*numNotWaited)++;
+	    } else {
+		(*numWaited)++;
+	    }
+            //printf("semValue: %d\n", semValue);
+	    sem_post(serviceDataSem);
+
+	    // Serve customer
             sem_wait(clerkSem);
             printf("%s getting helped\n", customers[i].name.c_str());
             if (customers[i].serviceTime > 0) {
@@ -71,8 +91,17 @@ int main(int argc, char* argv[]) {
 
     if (pid != 0) {
         while ((pid = wait(&status)) > 0);
+
+	printf("Total customers serviced: %d\n", *totalServiced);
+	printf("Number of customers with no wait: %d\n", *numNotWaited);
+	printf("Number of customers required to wait: %d\n", *numWaited);
+
+	// Close semaphores and shared memory
+        shmdt(totalServiced);
         sem_unlink("clerks");
         sem_close(clerkSem);
+        sem_unlink("serviceData");
+        sem_close(serviceDataSem);
     }
     return 0;
 }
